ea240e52d228750946a6f7c18fd9879e16b5b9e0
iProzza <59886338+iProzza@users.noreply.github.com>
2023-01-23 19:30:26 +0300

    Проект "отвязан" от репозитория GIT

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgramm.java
@@ -1,10 +1,10 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgramm{
+public class MyFirstProgramm {
 
 
-	public static void main (String[] args){
-		System.out.println("Hello, World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 
 }
