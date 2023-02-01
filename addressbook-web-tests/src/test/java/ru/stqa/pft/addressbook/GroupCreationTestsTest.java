b20e4057c3127ed03b665234aa9615850df370b6
iProzza <59886338+iProzza@users.noreply.github.com>
2023-02-01 20:11:07 +0300

    создание нов.контакта Test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTestsTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTestsTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
 
 public class GroupCreationTestsTest {
